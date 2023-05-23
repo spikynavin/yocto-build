f547240e4e3d5eaac74ad51499434d5fb17540e5
Naveen <spikynavin@gmail.com>
2023-05-24 00:57:35 +0530

    fixing the build version 0.1

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 
 echo "hello world for yocto testing in workflows...!"
 
-apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm python3-subunit mesa-common-dev
-
 mkdir -p /home/$USER/yocto
 
 cd /home/$USER/yocto
