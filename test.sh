6abd559dee6d7f8ad7ab7b82fad316f9ead06196
Naveen <spikynavin@gmail.com>
2023-05-24 01:02:33 +0530

    bitbake function added

--- test.sh
+++ test.sh
@@ -2,8 +2,20 @@
 
 echo "hello world for yocto testing in workflows...!"
 
+echo "Current path: $(pwd)"
+
 mkdir -p /home/$USER/yocto
 
 cd /home/$USER/yocto
 
+git config --global user.name "spikynavin"
+git config --global user.email "spikynavin@gmail.com"
+git config --list
+
 git clone -b dunfell git://git.yoctoproject.org/poky.git
+
+cd /home/$USER/yocto/poky
+
+source oe-init-build-env build
+
+bitbake -s
