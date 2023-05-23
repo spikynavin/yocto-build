9e052113a76d9b6c965dff548ec0cb3a6b9f198b
Naveen <spikynavin@gmail.com>
2023-05-24 00:43:40 +0530

    script updated

--- test.sh
+++ test.sh
@@ -2,4 +2,10 @@
 
 echo "hello world for yocto testing in workflows...!"
 
-lsb_release -a
+apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm python3-subunit mesa-common-dev
+
+mkdir -p /home/$USER/yocto
+
+cd /home/$USER/yocto
+
+git clone -b dunfell git://git.yoctoproject.org/poky.git
